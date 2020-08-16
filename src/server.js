1878c180b5b62057a732e84de912e2d0c35c3f40
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-08-16 16:53:17 -0300

    Feat: Give classes and Study pages connected to back end

--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ server.use(express.static("public"))
     return res.sendFile(__dirname + "/views/index.html")             
 })
 .get("/study", (req, res)=> {
-    return res.send("Study") 
+    return res.sendFile(__dirname + "/views/study.html")
 })
+.get("/give-classes", (req, res)=> {
+    return res.sendFile(__dirname + "/views/give-classes.html")
+})
+ 
 .listen(5500)

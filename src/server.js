002394962605bd2ba3ec06900d3333079104aaca
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2020-08-18 09:36:22 -0300

    Fix: give-classes page loading

--- src/server.js
+++ src/server.js
@@ -24,14 +24,53 @@ const proffys = [
     },
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",  
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+function getSubject(subjectNumber){
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
 function pageLanding(req, res) {
     return res.render("index.html")
 } 
+
 function pageStudy(req, res) {
-    return res.render("study.html",  { proffys })
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
+
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html")
+    const data = req.query
+
+    const isNotEmpty = Object.keys(data).length > 0 
+
+    if (isNotEmpty) { 
+    data.subject = getSubject()
+    proffys.push(data)
+    }
+    
+    return res.render("give-classes.html", {subjects, weekdays})
 }
 
 const express = require('express')
@@ -43,7 +82,7 @@ nunjucks.configure('src/views', {
     noCache: true,
 })
 
-server
+server 
 //configuração de arq. estáticos
 .use(express.static("public"))
 //rotas da aplicação
